5a2189109bda07d57417c5a307cc03b024ce8862
이재광 <yellow@wordncode.com>
2026-07-13 15:03:03 +0900

    fix: Tailwind CSS v4와 충돌하는 vite-plugin-purgecss 플러그인 제거 (스타일시트 누락 및 화면 깨짐 문제 해결)

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
-import purgecss from 'vite-plugin-purgecss'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/react_dashboard_01/',
   plugins: [
     react(),
-    tailwindcss(),
-    (purgecss({
-      content: [
-        './index.html',
-        './src/**/*.html',
-        './src/**/*.tsx',
-        './src/**/*.ts'
-      ],
-      safelist: {
-        standard: ['html', 'body', 'dark'],
-        deep: [/dark$/] // 다크모드 대응 스타일 클래스 탈락 예방
-      }
-    }) as any)
+    tailwindcss()
   ],
 })
+
